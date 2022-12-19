c1478d951795aef02808ec87bc45e434434e8b46
arturasmckwcz <arturasmckwcz@gmail.com>
2022-12-19 21:21:38 +0100

    updated components to check queries

--- src/components/pages/Quiz.tsx
+++ src/components/pages/Quiz.tsx
@@ -7,7 +7,7 @@ const Quiz = () => {
     <>
       <h1>This is the Quiz!</h1>
       <pre>quiz: {JSON.stringify(quiz, null, 2)}</pre>
-      <pre>loading: {loading}</pre>
+      <pre>loading: {String(loading)}</pre>
       <pre>error: {JSON.stringify(error, null, 2)}</pre>
     </>
   );
--- src/components/pages/Verdict.tsx
+++ src/components/pages/Verdict.tsx
@@ -1,7 +1,20 @@
-const Verdict = () => (
-  <>
-    <h1>This is the Verdict!</h1>
-  </>
-);
+import { useVerdictQuery } from '../../graphql/verdict.generated';
+
+const Verdict = () => {
+  const {
+    data: verdict,
+    loading,
+    error,
+  } = useVerdictQuery({ variables: { score: 60 } });
+
+  return (
+    <>
+      <h1>This is the Verdict!</h1>
+      <pre>verdict: {JSON.stringify(verdict, null, 2)}</pre>
+      <pre>loading: {String(loading)}</pre>
+      <pre>error: {JSON.stringify(error, null, 2)}</pre>
+    </>
+  );
+};
 
 export default Verdict;
