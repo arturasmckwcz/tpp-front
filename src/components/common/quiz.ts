01c537a679d878879a749e3768a0e696bd76343b
arturasmckwcz <arturasmckwcz@gmail.com>
2022-12-22 14:40:56 +0100

    clean up

--- src/components/common/quiz.ts
+++ src/components/common/quiz.ts
@@ -11,31 +11,3 @@ export const quizInit = (countQuestions: number): Quiz => ({
   current: 0,
   score: 0,
 });
-
-export const questions = [
-  {
-    id: 1,
-    question:
-      'Is this the question. Rggdaojfkjsdvd jgaofnjac jfakjvbjc jdfjbckjBCJB jfbkjbjbck lDJFBlkjdbkJBCK kjdhjkbfh hdbfjhfjhfjH SDHJFJHFDJHBFJH JDHFJH?',
-    answers: [
-      {
-        answer:
-          'Yes, it is the question Rggdaojfkjsdvd jgaofnjac jfakjvbjc jdfjbckjBCJB jfbkjbjbck lDJFBlkjdbkJBCK kjdhjkbfh',
-        points: 10,
-      },
-      {
-        answer:
-          'No, it is not the question Rggdaojfkjsdvd jgaofnjac jfakjvbjc jdfjbckjBCJB jfbkjbjbck lDJFBlkjdbkJBCK kjdhjkbfh',
-        points: 20,
-      },
-    ],
-  },
-  {
-    id: 2,
-    question: 'Is this the answer?',
-    answers: [
-      { answer: 'Yes, it is the answer', points: 30 },
-      { answer: 'No, it is not the answer', points: 40 },
-    ],
-  },
-];
