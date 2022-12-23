d43c294513ac068170980d00489680602b61986a
arturasmckwcz <arturasmckwcz@gmail.com>
2022-12-23 20:31:12 +0100

    got rid of useEffect

--- src/components/pages/Start.tsx
+++ src/components/pages/Start.tsx
@@ -1,11 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Space } from 'antd';
 
 import { Quiz, quizInit } from '../common/quiz';
 import { getQuizStatus, QuizStatus } from '../common/helpers';
 import { QuestionWithAnswers } from '../../graphql/schema.generated';
-import { useQuestionsWithAnswersLazyQuery } from '../../graphql/quiz.generated';
-import { useEffect } from 'react';
+import { useQuestionsWithAnswersQuery } from '../../graphql/quiz.generated';
 
 type Props = {
   questions: QuestionWithAnswers[];
@@ -16,56 +15,53 @@ type Props = {
 
 const Start = ({ questions, setQuestions, quiz, setQuiz }: Props) => {
   const navigate = useNavigate();
-  const [fetchQuiz] = useQuestionsWithAnswersLazyQuery();
-
-  useEffect(() => {
-    (async () => {
-      if (!questions.length) {
-        try {
-          const data = (await fetchQuiz()).data
-            ?.questionsWithAnswers as QuestionWithAnswers[];
-          setQuestions(data);
-        } catch (error) {}
-      }
-    })();
-  }, [fetchQuiz, questions.length, setQuestions]);
+  const { loading, error } = useQuestionsWithAnswersQuery({
+    onCompleted: data => {
+      setQuestions(data.questionsWithAnswers as QuestionWithAnswers[]);
+    },
+  });
 
   return (
     <>
       <h1>Are you an introvert or an extrovert?</h1>
-      <Form
-        name='basic'
-        labelCol={{ span: 2 }}
-        wrapperCol={{ span: 16 }}
-        onFinish={formData => {
-          setQuiz({
-            ...quizInit(questions.length),
-            email: formData.email,
-          });
-          navigate('/quiz');
-        }}
-        autoComplete='off'
-      >
-        <Form.Item
-          label='Email'
-          name='email'
-          rules={[
-            {
-              required: true,
-              type: 'email',
-              message: "Please make sure you're submitting an email",
-            },
-          ]}
+      {loading && <Space>Loading...</Space>}
+      {error ? (
+        <Space>Fetching failed.</Space>
+      ) : (
+        <Form
+          name='basic'
+          labelCol={{ span: 2 }}
+          wrapperCol={{ span: 16 }}
+          onFinish={formData => {
+            setQuiz({
+              ...quizInit(questions.length),
+              email: formData.email,
+            });
+            navigate('/quiz');
+          }}
+          autoComplete='off'
         >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label='Email'
+            name='email'
+            rules={[
+              {
+                required: true,
+                type: 'email',
+                message: "Please make sure you're submitting an email",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 2, span: 16 }}>
-          <Button type='primary' htmlType='submit'>
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item wrapperCol={{ offset: 2, span: 16 }}>
+            <Button type='primary' htmlType='submit'>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
       {getQuizStatus(quiz) === QuizStatus.IN_PROGRESS && (
         <>
           <p>Or you may return to the questions</p>
